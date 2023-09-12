13060edabc18dffd997e1b675dc8788ac239b3e8
lualyssa <117121956+lualyssa@users.noreply.github.com>
2023-09-12 00:30:28 -0400

    new Multiples class body

--- src/Multiples.java
+++ src/Multiples.java
@@ -1,6 +1,13 @@
 public class Multiples {
     public static void main(String[] args) {
+        int numMultiples = 0;
 
-        System.out.println();
+        for(int i = 0; i < 1000; i++){
+            if (i % 3 == 0 || i % 5 == 0){
+                numMultiples += 1;
+            }
+        }
+        System.out.println("There are " + numMultiples + " multiples of 3 or 5 below 1000.");
+
     }
 }
